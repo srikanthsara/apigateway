ba7b0c178231fcb7a221890461fc41b41c7a3d7c
Srikanth Sara <srikanth.wankar@gmail.com>
2026-06-09 02:33:35 +0100

    added aditional classes for APIGateway and tempararly disabled authentication & authorization

--- src/main/java/com/gateway/config/CorsConfig.java
+++ src/main/java/com/gateway/config/CorsConfig.java
@@ -3,20 +3,38 @@ package com.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",config);
+
+        CorsConfiguration config =
+                new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(
+                List.of("*"));
+
+        config.setAllowedMethods(
+                List.of("*"));
+
+        config.setAllowedHeaders(
+                List.of("*"));
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                config);
+
         return new CorsWebFilter(source);
     }
 }
